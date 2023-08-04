d71efb42926b0dc250cfb1b9d9bd6f2517ae469e
n1colashka <deckards2505@gmail.com>
2023-08-04 21:15:16 +0600

    add order, 404 pages

--- src/js/components/cart.js
+++ src/js/components/cart.js
@@ -1,4 +1,5 @@
 const cartCheckbox = document.querySelector('.cart__checkbox');
+const formBoxItems = document.querySelectorAll('.form-box');
 
 if (cartCheckbox) {
   const cartItemCheckboxes = document.querySelectorAll('.cart-item__checkbox');
@@ -10,6 +11,25 @@ if (cartCheckbox) {
       const input = el.querySelector('input');
 
       input.checked = cartCheckboxInput.checked;
+    })
+  })
+}
+
+if (formBoxItems.length > 0) {
+  formBoxItems.forEach(el => {
+    const input = el.querySelector('input');
+
+    input.addEventListener('change', (e) => {
+      const parent = input.closest('.form-boxes');
+      const active = parent.querySelector('.form-box.is-active');
+
+      if (active) {
+        active.classList.remove('is-active');
+      }
+
+      if (input.closest('.form-box--expand')) {
+        input.closest('.form-box').classList.add('is-active');
+      }
     })
   })
 }
--- src/js/components/tabs.js
+++ src/js/components/tabs.js
@@ -4,6 +4,7 @@ import { setCenter } from "./map";
 initTabs('.catalog__tabs li', '.catalog__list');
 initTabs('.terms-tabs li', '.terms-content__item');
 initTabs('.contacts__tabs li', '.contacts__info', onMapTabChange);
+initTabs('.order__tabs li', '.order__tabs-content');
 
 function onMapTabChange(tab) {
   setCenter(tab.dataset.coords);
